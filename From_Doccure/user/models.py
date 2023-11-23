55750506ee88e7d142fd853623b276246b1fd125
salman <salmanmdsultan@gmail.com>
2023-11-23 12:27:26 +0600

    first commit

---
+++ From_Doccure/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+# class user_registration(models.Model):
+#     Full_Name = models.CharField(max_length=100)
+#     Email = models.EmailField()
+#     Mobile = models.IntegerField()
+#     description = models.TextField()
+#     brand = models.CharField(max_length=100)
+   
