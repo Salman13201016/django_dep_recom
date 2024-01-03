bb0b9cf6cfd894ee1e2fbd37f5c8dab4ab2abab0
MANIRUZZAMAN <118476008+Manir96@users.noreply.github.com>
2024-01-03 10:27:55 +0600

    disease curd operation done

--- From_Doccure/dashboard_from/urls.py
+++ From_Doccure/dashboard_from/urls.py
@@ -6,7 +6,7 @@ from . import views as disease
 
 
 urlpatterns = [
-    path('', department.name_panel,name='home_page'),
+    path('', department.name_panel,name='home'),
     path('home/', department.dep_name_panel,name='department'),
     path('store/', department.department_name_store,name='department_stor'),
     path('store/edit/<int:id>', department.edit_depart_name,name='edit_depar'),
@@ -16,5 +16,9 @@ urlpatterns = [
     
     path('di_panel/', disease.disease_panel,name='di_panel'),
     path('sub_store/', disease.disease_sub_store,name='sub_store'),
+    path('edit_disease/<int:id>', disease.edit_disease,name='edit_disease'),
+    path('edit_disease/update/', disease.update_disease,name='update_disease'),
+    path('edit_disease/delete/<int:id>', disease.delete_disease,name='delete_disease'),
+    
     
 ]
